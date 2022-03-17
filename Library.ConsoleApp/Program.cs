b1a04c21017737106e1171b54ca2b49b31a6200a
Quentin Savean <quentin.savean@etu.univ-lehavre.fr>
2022-03-17 18:51:54 +0100

    MAJ

--- Library.ConsoleApp/Program.cs
+++ Library.ConsoleApp/Program.cs
@@ -12,7 +12,8 @@ namespace Library.ConsoleApp
         {
             TestBLL testBLL = new TestBLL();
             testBLL.DisplayAllMoviesOrdered();
-
+            testBLL.DisplayMoviesBeforeDate(new DateTime(2009, 12, 25));
+            testBLL.DisplayMoviesBeforeYear(2018);
             Console.ReadKey();
         }
     }
--- Library.ConsoleApp/TestBLL.cs
+++ Library.ConsoleApp/TestBLL.cs
@@ -28,6 +28,97 @@ namespace Library.ConsoleApp
             Console.WriteLine("******************** TestBLL - DisplayAllMoviesOrdered ********************");
             DisplayMovies(list);
         }
+
+        public void DisplayMoviesBeforeDate(DateTime date)
+        {
+            List<Movie> list;
+
+            MovieCollection movies = new MovieCollection();
+            movies.GetAll();
+
+            /*list = movies
+                .OrderBy(movie => movie.Title)
+                .ToList();*/
+
+            list = (from movie in movies
+                    where movie.Date < date
+                    orderby movie.Title ascending
+                    select movie).ToList();
+
+            Console.WriteLine("******************** TestBLL - DisplayMoviesBeforeDate ********************");
+            DisplayMovies(list);
+        }
+
+        public void DisplayMoviesBeforeYear(int year)
+        {
+            List<Movie> list;
+
+            MovieCollection movies = new MovieCollection();
+            movies.GetAll();
+
+            list = (from movie in movies
+                    where movie.Date.Year < year
+                    orderby movie.Title ascending
+                    select movie).ToList();
+
+            Console.WriteLine("******************** TestBLL - DisplayMoviesBeforeYear ********************");
+            DisplayMovies(list);
+        }
+
+        public void DisplayMoviesByGenreName(string genreName)
+        {
+            List<Movie> list;
+
+            MovieCollection movies = new MovieCollection();
+
+            movies.GetAll();
+
+            list = (from movie in movies
+                    from genre in movie.Genres
+                    where genre.Name == genreName
+                    orderby movie.Title ascending
+                    select movie).ToList();
+
+            Console.WriteLine("******************** TestBLL - DisplayMoviesByGenreName ********************");
+            DisplayMovies(list);
+        }
+
+        public void DisplayMoviesCountByGenre()
+        {
+            List<Movie> list;
+
+            GenreCollection genres = new GenreCollection();
+
+            genres.GetAll();
+
+            Console.WriteLine("******************** TestBLL - DisplayMoviesCountByGenre ********************");
+            foreach ( Genre genre in genres.OrderBy(g => g.Name))
+            {
+                MovieCollection movies = new MovieCollection();
+                movies.Get_ByGenreId(genre.Id);
+
+                string displayGenre = $"Genre : {genre.Name}, nb movies : {movies.Count}";
+            }
+
+            Console.WriteLine();
+
+            var list = (from genre in genres
+                        orderby genre.Name ascending
+                        select new
+                        {
+                            genre.Name,
+                            genre.Movies.Count
+                        }).ToList();
+
+
+            foreach ( var item in list)
+            {
+                Console.WriteLine($"Genre :" { item.Name }, )
+            }
+
+            DisplayMovies(list);
+        }
+
         private void DisplayMovies(List<Movie> movies)
         {
             foreach (Movie movie in movies)
